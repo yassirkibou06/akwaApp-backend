120867906a3d845b15c30c7521d533d7f11b4052
yassirkibou06 <yassirkibou06@gmail.com>
2023-01-03 11:58:30 +0100

    sd

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors({}))
+app.use(
+    cors({
+      origin: ["http://localhost:3000", "https://akwa-backend.onrender.com"],
+    })
+  );
 
 mongoose.connect(process.env.MOGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`)))
