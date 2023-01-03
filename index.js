5f2e40719737bc12e2bb6d34b829f0baed68780a
yassirkibou06 <yassirkibou06@gmail.com>
2023-01-03 09:34:35 +0100

    endapi

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const axios = require('axios')
 const bodyParser = require('body-parser')
 const list = require('./List.json')
 const Categories = require('./Categories.json')
-
+const detail = require('./Details.json')
 
 const app = express();
 
@@ -15,182 +15,13 @@ app.get('/', (req, res) => {
     res.send('Welcome to my api');
 })
 
-////GET Products Categories//////
-/*app.get('/categories/list', async (req, res) => {
-    res.send(
-    [
-        {
-            id: 1,
-            cateName: "clothing"
-        },
-        {
-            id:2,
-            cateName: "shoes"
-        },
-        {
-            id:3,
-            cateName: "bestsellers"
-        }
-    ]
-    )
-})*/
-
-// GET Products List ///
-/*
-app.get(`/products/list/:gender/:categorie`, async (req, res) => {
-    const { gender } = req.params;
-    const { categorie } = req.params;
-
-
-    axios.get(`https://www.theoutnet.com/en-us/shop/${gender}/${categorie}`)
-        .then(response => {
-            const html = response.data;
-            const $ = cheerio.load(html);
-
-            const Products = []
-            $('.ProductItem24.ProductList52__productItem').map((i, el) => {
-                const productId = $(el).find('.ProductItem24__details.ProductItem24__details--brief').attr('data-product-id');
-                const name = $(el).find('.ProductItem24__name').text();
-                const brandName = $(el).find('.ProductItem24__designer').text();
-                const redPrice = $(el).find('span[content]').text();
-                const whitePrice = $(el).find('.PriceWithSchema9__wasPrice').text();
-                const discount = $(el).find('.PriceWithSchema9__discount.PriceWithSchema9__discount--sale').text();
-                const imageUrl = $(el).find('.AspectRatio18__content img').attr('src');
-                const imageSecond = $(el).find('.DoubleImage18.secondaryImage img').attr('src');
-                Products.push({
-                    productId,
-                    name,
-                    brandName,
-                    redPrice,
-                    whitePrice,
-                    discount,
-                    imageUrl,
-                    imageSecond
-                })
-            })
-            res.json(Products)
-        })
-})
-
-//// Get products list for women ///
-app.get(`/products/list/:categorie`, async (req, res) => {
-    const { categorie } = req.params;
-
-
-    axios.get(`https://www.theoutnet.com/en-us/shop/${categorie}`)
-        .then(response => {
-            const html = response.data;
-            const $ = cheerio.load(html);
-
-            const Products = []
-            $('.ProductItem24__p').map((i, el) => {
-                const productId = $(el).find('.ProductItem24__details.ProductItem24__details--brief').attr('data-product-id');
-                const name = $(el).find('.ProductItem24__name').text();
-                const brandName = $(el).find('.ProductItem24__designer').text();
-                const redPrice = $(el).find('span[content]').text();
-                const whitePrice = $(el).find('.PriceWithSchema9__wasPrice').text();
-                const discount = $(el).find('.PriceWithSchema9__discount.PriceWithSchema9__discount--sale').text();
-                const imageUrl = $(el).find('img').attr('src');
-                const imageSecond = $(el).find('.DoubleImage18.secondaryImage img').attr('src');
-                Products.push({
-                    productId,
-                    name,
-                    brandName,
-                    redPrice,
-                    whitePrice,
-                    discount,
-                    imageUrl,
-                    imageSecond
-                })
-            })
-            res.json(Products)
-        })
-})
-*/
-
 /// Products Details ///
-app.get('/products/detail/:productId', async (req, res) => {
-    const { productId } = req.params;
-
-    axios.get(`https://www.theoutnet.com/en-us/shop/product/${productId}`)
-        .then(response => {
-            const html = response.data;
-            const $ = cheerio.load(html);
-
-            const Products = []
-            $('.ProductDetailsPage86__wrapper').map((i, el) => {
-                const name = $(el).find('.ProductInformation86__name').text();
-                const brandName = $(el).find('.ProductInformation86__designer span').text();
-                const redPrice = $(el).find('.PriceWithSchema9__value.PriceWithSchema9__value--sale.PriceWithSchema9__value--details span').text();
-                const whitePrice = $(el).find('.PriceWithSchema9__discountContainer s').text();
-                const discount = $(el).find('div.PriceWithSchema9__discount.PriceWithSchema9__discount--sale.PriceWithSchema9__discount--details').text();
-                const imageUrl1 = $(el).find('div.ImageCarousel86__thumbnails:nth-child(1) img').attr('src');
-                const SizeOne = $(el).find('ul.GridSelect11 li:nth-child(1) .GridSelect11__optionBox').text();
-                const SizeTwo = $(el).find('.GridSelect11 li:nth-child(2) .GridSelect11__optionBox').text();
-                const SizeThree = $(el).find('.GridSelect11 li:nth-child(3) .GridSelect11__optionBox').text();
-                const SizeFour = $(el).find('.GridSelect11 li:nth-child(4) .GridSelect11__optionBox').text();
-                const SizeFive = $(el).find('.GridSelect11 li:nth-child(5) .GridSelect11__optionBox').text();
-                const AllSize = [];
-                AllSize.push({ SizeOne, SizeTwo, SizeThree, SizeFour, SizeFive });
-               Products.push({
-                    name,
-                    brandName,
-                    redPrice,
-                    whitePrice,
-                    discount,
-                    imageUrl1,
-                    AllSize
-                })
-            })
-            res.send(Products)
-        })
-})
-/*
-
-///GET Products List types ///
-app.get('/products/list/:gender/:cate/:type', async (req, res) => {
-    const { gender } = req.params;
-    const { cate } = req.params;
-    const { type } = req.params;
-
-
-    axios.get(`https://www.theoutnet.com/en-us/shop/${gender}/${cate}/${type}`)
-        .then(response => {
-            const html = response.data;
-            const $ = cheerio.load(html);
-
-            const Products = [];
-            $('.ProductItem24__p').map((i, el) => {
-                const productId = $(el).find('.ProductItem24__details.ProductItem24__details--brief').attr('data-product-id');
-                const name = $(el).find('.ProductItem24__name').text()
-                const brandName = $(el).find('.ProductItem24__designer').text();
-                const redPrice = $(el).find('span[content]').text();
-                const whitePrice = $(el).find('.PriceWithSchema9__wasPrice').text();
-                const discount = $(el).find('.PriceWithSchema9__discount.PriceWithSchema9__discount--sale').text();
-                const imageUrl = $(el).find('img').attr('src');
-                const imageSecond = $(el).find('.DoubleImage18.secondaryImage img').attr('src');
-                Products.push({
-                    productId,
-                    name,
-                    brandName,
-                    redPrice,
-                    whitePrice,
-                    discount,
-                    imageUrl,
-                    imageSecond
-                })
-            })
-            res.json(Products);
-        })
-})
-*/
-
+/////////////////
 //////////////////////////////////////////////////////
 ///
 //
 //// GET Products List Men ///
-
-app.get('/products/list/men/clothing', async (req, res) => {
+app.get('products/list/men/clothing', async (req, res) => {
     res.json(list.clothingMen);
 })
 app.get('/products/list/men/shoes', async (req, res) => {
@@ -207,10 +38,10 @@ app.get('/products/list/women/clothing', async (req, res) => {
 app.get('/products/list/women/shoes', async (req, res) => {
     res.json(list.shoesWomen);
 })
-app.get('/products/list/men/bestsellers', async (req, res) => {
+app.get('/products/list/women/bestsellers', async (req, res) => {
     res.json(list.bestSellersWomen);
 })
-////Gategorei (jackets/shorts/pants/jeans) Men///
+////Gategorie (jackets/shorts/pants/jeans) Men///
 app.get('/products/list/men/jackets', async (req, res) => {
     res.json(Categories.jacketsMen);
 })
@@ -242,33 +73,16 @@ app.get('/products/list/women/pants', async (req, res) => {
 app.get('/products/list/women/jeans', async (req, res) => {
     res.json(Categories.jeansWomen);
 })
+
+////detail products
 app.get('/products/details/:id', async (req, res) => {
-    res.send([
-        {
-            "name": "Jack cotton-jersey shirt",
-            "brandName": "RAG & BONE",
-            "redPrice": "$147",
-            "whitePrice": "$295",
-            "discount": "50% off",
-            "imageUrl1": "//www.theoutnet.com/variants/images/1647597298277411/F/w1020_q80.jpg",
-            "AllSize": [
-                {
-                    "SizeOne": "XXS",
-                    "SizeTwo": "XS",
-                    "SizeThree": "S",
-                    "SizeFour": "M",
-                    "SizeFive": "L"
-                }
-            ]
-        }
-    ])
+    const id = req.params;
+    
+    detail.map((el) => {
+        res.send(el[id.id])
+    })
 })
 
 app.listen(PORT, () => console.log(`start running on port ${PORT}`));
 
-//app.use(express.static(path.resolve(__dirname, '../frontend/build')))
-
-//app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
-//  });
 
