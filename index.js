80e187760cbed8a6128cce95d1b1de362a044f78
yassirkibou06 <yassirkibou06@gmail.com>
2023-01-03 13:24:01 +0100

    aaaa

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
   });
 
 mongoose.connect(process.env.MOGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
